e0449827fb9bb857a869a9d5bc73b8e28e0e010b
fengchufu <fengchufu@163.com>
2017-05-26 17:57:02 +0800

    commit test03

---
+++ PythonApplication1/PythonApplication1/Test03.py
@@ -0,0 +1,20 @@
+#!/urs/bin/env python3
+# -*- coding: utf-8 -*-
+
+'a test module'
+
+_author_ = 'Finley'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args)==1:
+        print("Hello,world!")
+    elif len(args)==2:
+        print('Hello, %s!' %args[1])
+    else:
+        print('Too many arguments')
+
+if __name__=='__main__':
+    test()
